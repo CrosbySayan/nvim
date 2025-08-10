799f7e19c65fd6539341c89618a3e2d0ecf1537b
Crosby Thinkpad <crosbysayan2023@gmail.com>
2025-08-10 13:52:56 -0400

    completions

--- lua/crosby/plugins/gruvbox.lua
+++ lua/crosby/plugins/gruvbox.lua
@@ -23,16 +23,16 @@ return {
         invert_tabline = false,
         invert_intend_guides = false,
         inverse = true, -- invert background for search, diffs, statuslines and errors
-        contrast = "", -- can be "hard", "soft" or empty string
+        contrast = "",  -- can be "hard", "soft" or empty string
         palette_overrides = {},
         overrides = {},
         dim_inactive = false,
-        transparent_mode = false,
+        transparent_mode = true,
       })
-      
+
       -- Set colorscheme
       vim.cmd("colorscheme gruvbox")
-      
+
       -- Optional: Set background to dark or light
       vim.o.background = "dark" -- or "light"
     end,
