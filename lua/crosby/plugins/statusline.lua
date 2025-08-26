3a2d1782e3011c839ab14629a6f645d628d51953
Crosby Thinkpad <crosbysayan2023@gmail.com>
2025-08-26 00:23:44 -0400

    Lualine for git branch, and harpoon integration soon

--- lua/crosby/plugins/statusline.lua
+++ lua/crosby/plugins/statusline.lua
@@ -1,9 +1,43 @@
 return {
-  {
-    'echasnovski/mini.nvim',
-    config = function()
-      local statusline = require 'mini.statusline'
-      statusline.setup { use_icons = true }
-    end
-  }
+  'nvim-lualine/lualine.nvim',
+  dependencies = {
+    {
+      'echasnovski/mini.icons',
+      config = function()
+        require('mini.icons').setup()
+        -- Make mini.icons compatible with nvim-web-devicons API
+        MiniIcons.mock_nvim_web_devicons()
+      end
+    }
+  },
+  config = function()
+    require('lualine').setup {
+      options = {
+        theme = 'gruvbox-material',
+        component_separators = '|',
+        section_separators = '',
+      },
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch' },
+        lualine_c = { 'filename', 'diagnostics' },
+        lualine_x = {
+          -- Simple LSP indicator
+          {
+            function()
+              local clients = vim.lsp.get_clients()
+              if next(clients) == nil then
+                return ''
+              end
+              return ' LSP'
+            end,
+            color = { fg = '#a9b665' }, -- Green color when active
+          },
+          'filetype'
+        },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' }
+      },
+    }
+  end
 }
